22850a97061e43c2d4f24fbdfe4d70942c9a492b
LX2010JY <LX2010JY@163.com>
2016-11-25 18:01:29 +0800

    11-22 微改

--- spider/imooc/spidermain.py
+++ spider/imooc/spidermain.py
@@ -44,7 +44,8 @@ class SpiderMain(object):
         print(u'课程名称：',self.res_datas[0].subject)
         for res_data in self.res_datas:
             print(u'------->',res_data.filename)
-        state = input(u'选择清晰度（1：超清UHD，2：高清HD，3：普清SD）：'.encode('gbk'))
+        state = input(u'选择清晰度（1：超清UHD，2：高清HD，3：普清SD）：')
+        state = int(state)
         if state not in [1,2,3]:
             print(u'输入有误.')
             return
